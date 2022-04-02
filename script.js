a4cc0c78abbf0179cf1287992dbc4467b5508d1e
Rahul K R <rahulramakrishnan512@gmail.com>
2022-04-02 11:52:43 +0530

    Calculator done

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function buttonclick(val){
     document.getElementById("screen").value+=val;
 }
 function clearDisplay(val){
-    document.getElementById("screen").value=""
+    document.getElementById("screen").value="";
 
 }
 function equalClick(){
     var text=document.getElementById("screen").value
     var result=eval(text)
     document.getElementById("screen").value=result
+    
 }
 function del(){
     document.getElementById("screen").value=document.getElementById("screen").value.slice(0,-1);
@@ -18,7 +19,7 @@ function calc(){
     try{
         document.getElementById("screen").value=eval(document.getElementById("screen").value);
     }
-    catch(err)
+    catch(SyntaxError)
     {
         alert("Syntax error")
     }
